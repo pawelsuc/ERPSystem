bec043b4d7fbc9da472edd6ed3e80b5454af607b
Paweł Suchań <pawelsuchan94@gmail.com>
2023-03-15 07:47:42 +0100

    Change AppController.java

--- skJavaFx/src/main/java/com/example/skjavafx/controller/AppController.java
+++ skJavaFx/src/main/java/com/example/skjavafx/controller/AppController.java
@@ -1,9 +1,12 @@
 package com.example.skjavafx.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,8 +22,17 @@ public class AppController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("initialize");
+        loadView();
 
+    }
+
+    private void loadView() {
+        try {
+            BorderPane borderPane = FXMLLoader.load(getClass().getResource("/com/example/skjavafx/employee.fxml"));
+            appPane.getChildren().add(borderPane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
